6937b1be40ce4199f2609f2861c11dd7e785ed97
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-13 16:32:39 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.backend.name
+}
+
+output "aws_region" {
+  value = "ap-south-1"
+}
