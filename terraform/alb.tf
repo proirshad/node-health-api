fb6d8e859a1476fd8246b9b5b9210cce7d09e86c
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-13 16:35:36 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,8 +1,27 @@
+resource "aws_security_group" "alb_sg" {
+  name   = "alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lb" "backend" {
   name               = "backend-alb"
-  internal           = false
   load_balancer_type = "application"
   subnets            = var.subnet_ids
+  security_groups    = [aws_security_group.alb_sg.id]
 }
 
 resource "aws_lb_target_group" "backend" {
