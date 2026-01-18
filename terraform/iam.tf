cd447c97064e86eabc41ace618207f7c05e54b3b
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-18 19:36:46 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,15 +3,42 @@ resource "aws_iam_role" "ecs_execution" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ecs-tasks.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::288842392399:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:KalpeshVadher/Azodha:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
   })
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_policy" {
   role       = aws_iam_role.ecs_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
