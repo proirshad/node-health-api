261316accf816ed9ba5f373b59aeb2d6796919ad
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-18 17:51:50 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "ecs_task_execution" {
+resource "aws_iam_role" "ecs_execution" {
   name = "ecs-task-execution-role"
 
   assume_role_policy = jsonencode({
@@ -11,20 +11,7 @@ resource "aws_iam_role" "ecs_task_execution" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution.name
+resource "aws_iam_role_policy_attachment" "ecs_policy" {
+  role       = aws_iam_role.ecs_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs-task-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ecs-tasks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
 }
