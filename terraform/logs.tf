809419d351185b974d90b447291eb41d4fd4bb0c
proirshad <64773022+proirshad@users.noreply.github.com>
2026-02-13 21:07:24 +0530

    Containerize & Deploy a Simple Servic

--- terraform/logs.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "ecs" {
-  name              = "/ecs/simple-backend"
-  retention_in_days = 7
-
-  lifecycle {
-    prevent_destroy = false      # safe destroy
-    ignore_changes  = [name]     # agar log group already exist, ignore
-  }
-}
--- terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-docker_image = "dockerhub_username/simplebackend:latest"
-aws_region   = "ap-south-1"
-vpc_cidr     = "10.0.0.0/16"
-public_subnet_count = 2
